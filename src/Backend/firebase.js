c21dc28532984350e4a5585c2baa46a0312acbbe
Anthony <aleung4@wisc.edu>
2019-03-13 15:06:59 -0500

    Firebase config and new Google account credential

---
+++ src/Backend/firebase.js
@@ -0,0 +1,11 @@
+const firebase = require('firebase')
+
+var config = {
+    apiKey: "AIzaSyArrokhuYfs5rPYBjqQmY3K9DeRHxLThBg",
+    authDomain: "codeshack-2a355.firebaseapp.com",
+    databaseURL: "https://codeshack-2a355.firebaseio.com",
+    projectId: "codeshack-2a355",
+    storageBucket: "codeshack-2a355.appspot.com",
+    messagingSenderId: "220351087002"
+  };
+  firebase.initializeApp(config);
