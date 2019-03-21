a5f8191b655384f43dd85de7064b78aef5e2cc96
Sam Ware <sam.r.ware@gmail.com>
2019-03-21 09:47:05 -0500

    Add loadAllQuestions() action and reducer
    Also implement connect in Dashboard

--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -1,3 +1,17 @@
 //Here is where all the action creators will be
 import axios from "axios";
-const url = "URL to Backend API";
+const url = "http://localhost:8080/api/";
+
+export function loadAllQuestions() {
+    return dispatch => {
+        axios
+            .get(`${url}questions`)
+            .then(res => {
+                let questions = res.questions;
+                dispatch({ type: "LOAD_QUESTIONS", questions });
+            })
+            .catch(err => {
+                console.log("Error: Unable to get questions,", err);
+            });
+    };
+}
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import authUser from "./reducers/authUser";
+import question from "./reducers/question";
 import { connectRouter } from "connected-react-router";
 
 export default history =>
     combineReducers({
         authUser,
+        question,
         router: connectRouter(history)
     });
