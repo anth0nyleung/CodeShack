fc5290a99c5844f13021f48ee54bc820e20a8269
Sam Ware <sam.r.ware@gmail.com>
2019-03-21 11:22:49 -0500

    Implement Comment model and controller
    Add test for comment

--- src/Backend/Routes/index.js
+++ src/Backend/Routes/index.js
@@ -1,6 +1,8 @@
 const User = require("./user.route");
+const Comment = require("./comment.route");
 
 module.exports = router => {
     User(router);
+    Comment(router);
     // Add the other routes
 };
