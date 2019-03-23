5c024203d01436e77b745b50470d294e6d3aeead
Anthony <aleung4@wisc.edu>
2019-03-23 16:55:59 -0500

    Added routes for sign up page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
 import Dashboard from "./Components/Dashboard";
 import Login from "./Components/Login";
+import Signup from "./Components/Signup";
 
 class App extends Component {
     render() {
@@ -11,6 +12,8 @@ class App extends Component {
                     {/* Insert Routing here */}
                     <Route exact path="/dashboard" component={Dashboard} />
                     <Route exact path="/login" component={Login} />
+                    <Route exact path="/signup" component={Signup} />
+
                 </Switch>
             </div>
         );
