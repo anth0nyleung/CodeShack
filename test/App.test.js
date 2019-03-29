6e440947ff7e1f2d29e77ac17326473218cf2c8a
Samuel Ware <sam.r.ware@gmail.com>
2019-03-29 10:38:00 -0500

    Small change - remove contains add find

--- test/App.test.js
+++ test/App.test.js
@@ -2,35 +2,14 @@ import React from "react";
 import { configure, shallow } from "enzyme";
 import { expect } from "chai";
 import App from "../src/App";
-import { Switch, Route } from "react-router-dom";
-import Dashboard from "../src/Components/Dashboard";
-import Login from "../src/Components/Login";
-import Signup from "../src/Components/Signup";
-import CourseOverview from "../src/Components/CourseOverview";
 
 import Adapter from "enzyme-adapter-react-16";
 configure({ adapter: new Adapter() });
 
 describe("App component testing", () => {
-    it("it renders without crashing", done => {
+    it("it should render one div", done => {
         const wrapper = shallow(<App />);
-        expect(
-            wrapper.contains(
-                <div>
-                    <Switch>
-                        {/* Insert Routing here */}
-                        <Route exact path="/dashboard" component={Dashboard} />
-                        <Route exact path="/login" component={Login} />
-                        <Route exact path="/signup" component={Signup} />
-                        <Route
-                            exact
-                            path="/courses"
-                            component={CourseOverview}
-                        />
-                    </Switch>
-                </div>
-            )
-        ).to.equal(true);
+        expect(wrapper.find("div").length).to.equal(1);
         done();
     });
 });
