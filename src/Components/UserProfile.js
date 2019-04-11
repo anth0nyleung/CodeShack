cb62f8b911ffd609f08b951ef0bd61a265454012
Anthony <aleung4@wisc.edu>
2019-04-11 18:30:26 -0500

    Added tables

--- src/Components/UserProfile.js
+++ src/Components/UserProfile.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import { Jumbotron, Container } from "reactstrap";
+import { Row, Col, Button, Jumbotron, Container } from "reactstrap";
+import { BootstrapTable, TableHeaderColumn } from "react-bootstrap-table";
+
 
 const mapStateToProps = state => {
     return {
@@ -11,8 +13,10 @@ const mapStateToProps = state => {
 
 const imgStyles = {
     width: 50,
-    height: 50
-}
+    height: 50,
+    backgroundColor: "red",
+    justiftyContent : "center"
+};
 
 class UserProfile extends Component {
     constructor(props) {
@@ -34,11 +38,68 @@ class UserProfile extends Component {
                 <main>
                     <Jumbotron>
                         <Container>
-                            <h3>{this.props.username}</h3>
-                            <img src={this.state.url} style={imgStyles}/>
+                            <Row>
+                                <Col  sm="4"><h3 style={{margin: '5px',textAlign: "center"}}>{this.props.username}</h3></Col>
+                                <Col  sm="4"><img src={this.state.url} style={imgStyles}/></Col>
+                            </Row>
                             <hr/>
+                            <Button outline color="danger">Edit</Button>
                         </Container>
                     </Jumbotron>
+                    <Container>
+                    <h2>My Current Courses</h2>
+                    <BootstrapTable
+                            striped
+                            hover
+                            bordered={false}
+                        >
+                            <TableHeaderColumn
+                                isKey={true}
+                                dataField="index"
+                                width="40%"
+                            >
+                                Course Number
+                            </TableHeaderColumn>
+                            <TableHeaderColumn
+                                dataField="Question"
+                                width="50%"
+                            >
+                                Course Name
+                            </TableHeaderColumn>
+                        </BootstrapTable>
+                    <hr/>
+                    </Container>
+                    
+                    <Container>
+                        <h2>Recently Viewed Question</h2>
+                        <BootstrapTable
+                            striped
+                            hover
+                            bordered={false}
+                        >
+                            <TableHeaderColumn
+                                isKey={true}
+                                dataField="index"
+                                width="40%"
+                            >
+                                Index
+                            </TableHeaderColumn>
+                            <TableHeaderColumn
+                                dataField="Question"
+                                width="30%"
+                            >
+                                Question
+                            </TableHeaderColumn>
+                            <TableHeaderColumn
+                                dataField="Course"
+                                width="20%"
+                            >
+                                Course
+                            </TableHeaderColumn>
+                        </BootstrapTable>
+                    </Container>
+
+
                 </main>
                 <footer>
                     <Container>
